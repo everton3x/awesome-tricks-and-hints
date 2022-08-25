9857d190b2559647e15c909c1e02dc5adcd635fe
Everton da Rosa <everton3x@gmail.com>
2022-08-25 17:16:21 -0300

    Create pws-process.ps1

---
+++ PowerShell/pws-process.ps1
@@ -0,0 +1,8 @@
+Get-Process
+
+# Output
+# NPM(K)    PM(M)      WS(M)     CPU(s)      Id  SI ProcessName
+#  ------    -----      -----     ------      --  -- -----------
+#      11     2,75      11,30       0,00    1288   0 AggregatorHost
+#      19     6,77      15,14       0,00    2356   0 AppHelperCap
+#      22    17,03      35,38       1,05    7076   1 ApplicationFrameHost
