6688f9db210578aa887fc325f1da3d35a0abd627
Everton da Rosa <everton3x@gmail.com>
2022-08-25 17:15:01 -0300

    Update pws-version.ps1

--- PowerShell/pws-version.ps1
+++ PowerShell/pws-version.ps1
@@ -1,1 +1,14 @@
 $PSVersionTable
+
+# Output
+# Name                           Value
+# ----                           -----
+# PSVersion                      7.2.6
+# PSEdition                      Core
+# GitCommitId                    7.2.6
+# OS                             Microsoft Windows 10.0.25188
+# Platform                       Win32NT
+# PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0…}
+# PSRemotingProtocolVersion      2.3
+# SerializationVersion           1.1.0.1
+# WSManStackVersion              3.0
