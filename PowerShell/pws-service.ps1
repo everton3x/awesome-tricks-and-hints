728a66063556726d3be977cad97c163900f7f41a
Everton da Rosa <everton3x@gmail.com>
2022-08-25 17:17:09 -0300

    Create pws-service.ps1

---
+++ PowerShell/pws-service.ps1
@@ -0,0 +1,8 @@
+Get-Service
+
+# Output
+# Status   Name               DisplayName
+# ------   ----               -----------
+# Stopped  AarSvc_9223f6      Agent Activation Runtime_9223f6
+# Running  AdobeARMservice    Adobe Acrobat Update Service
+# Stopped  AJRouter           Serviço de Roteador AllJoyn
