cf6c7424a2780e39c3970d71a91d8773ee6aa957
Everton da Rosa <everton3x@gmail.com>
2022-08-30 07:21:04 -0300

    Create pws-download.ps1

---
+++ PowerShell/pws-download.ps1
@@ -0,0 +1,10 @@
+# Baixar arquivos
+
+# Não aceita FTP
+Invoke-WebRequest "caminho http/https" -OutFile "nome do arquivo.ext"
+
+# Aceita FTP
+powershell –c "(new-object System.Net.WebClient).DownloadFile('caminho do download', 'caminho de destino (pasta e arquivo)')"
+
+# ou
+(new-object System.Net.WebClient).DownloadFile("caminho do download", "caminho de destino (pasta e arquivo)")
